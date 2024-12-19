9a306dde7f4e8c11ce41f8cbc745ca91963dcacb
David S <davidsolisgallo@gmail.com>
2024-12-18 16:04:53 -0800

    sound of gun

--- b.cpp
+++ b.cpp
@@ -3,6 +3,9 @@
 int main()
 {
     std::cout << "This is the sound of the police b b b\n";
-    std::cout << "I am an ass man.\n"
+    std::cout << "I am an ass man.\n";
+    for(int i{0}; i < 10;i++){
+        std::cout << "ratata\n";
+    }
     return 0;
 }
