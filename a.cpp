d024d8d70af83f084337fc32f27c8cdca79cf19d
David S <davidsolisgallo@gmail.com>
2024-12-18 16:19:24 -0800

    removed a.cpp

--- a.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream> 
-
-int main()
-{
-    std::cout << "This is the a file\n";
-    std::cout << "This is the main file\n";
-    return 0;
-}
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+
+int main()
+{
+    std::cout << "I am combining a and b cpp files";
+        std::cout << "This is the sound of the police b b b\n";
+    std::cout << "I am an ass man.\n";
+    for(int i{0}; i < 10;i++){
+        std::cout << "ratata\n";
+        std::cout << "laser\n";
+    }
+    return 0;
+}
