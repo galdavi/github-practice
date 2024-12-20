7fefa6bda74cb5a96f9d22aac936f3b1c8e4ec5a
David S <davidsolisgallo@gmail.com>
2024-12-20 02:24:03 -0800

    Added fire physics
    Added fire physics so we can make the fire benders able to bend fire smoothly
    Classes affected:
    -Fire bender
    -Mage

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
-#include <iostream> 
+#include <iostream>
 
 int main()
 {
     std::cout << "I am combining a and b cpp files";
-        std::cout << "This is the sound of the police b b b\n";
+    std::cout << "This is the sound of the police b b b\n";
     std::cout << "I am an ass man.\n";
-    for(int i{0}; i < 10;i++){
+    for (int i{0}; i < 10; i++)
+    {
         std::cout << "ratata\n";
         std::cout << "laser\n";
     }
     std::cout << "Added etra fire\n";
+
+    std::cout << "This is the basic of git\n";
+    std::cout << "Water\n";
     return 0;
 }
